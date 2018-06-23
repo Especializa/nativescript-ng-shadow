9d5d4982a95281216b3cbedb775d942338039745
Josh Sommer <JoshDSommer@gmail.com>
2018-06-23 09:34:46 -0400

    fixing redundant module import

--- app/home/home.module.ts
+++ app/home/home.module.ts
@@ -1,23 +1,13 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from "./home-routing.module";
+import { HomeComponent } from "./home.component";
 
-import { NgShadowModule } from '../lib/src/public_api';
-
+import { NgShadowModule } from "../lib/src/public_api";
 
 @NgModule({
-    imports: [
-        NativeScriptModule,
-        HomeRoutingModule,
-        NgShadowModule,
-    ],
-    declarations: [
-        HomeComponent,
-    ],
-    schemas: [
-        NO_ERRORS_SCHEMA,
-    ],
+  imports: [HomeRoutingModule, NgShadowModule],
+  declarations: [HomeComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
-export class HomeModule { }
+export class HomeModule {}
