c3b3c147cecf9a0523bbd4634a115a1609e95e6c
Josh Sommer <JoshDSommer@gmail.com>
2018-06-23 09:30:24 -0400

    adding barrel for common folder and exporting it to consumers

---
+++ app/lib/src/nativescript-ngx-shadow/common/index.ts
@@ -0,0 +1,5 @@
+export * from './android-data.model';
+export * from './elevation.enum';
+export * from './ios-data.model';
+export * from './shadow';
+export * from './shape.enum';
--- app/lib/src/public_api.ts
+++ app/lib/src/public_api.ts
@@ -1,1 +1,2 @@
 export * from './lib.module';
+export * from './nativescript-ngx-shadow/common';
