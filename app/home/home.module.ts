259feaabb4cf9eade97a064d0df659e523c6cd5b
Josh Sommer <JoshDSommer@gmail.com>
2018-06-23 20:25:33 -0400

    updating lib path

--- app/home/home.module.ts
+++ app/home/home.module.ts
@@ -3,7 +3,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
 
-import { NgShadowModule } from "../lib/src/public_api";
+import { NgShadowModule } from "../../lib/src/public_api";
 
 @NgModule({
   imports: [HomeRoutingModule, NgShadowModule],
